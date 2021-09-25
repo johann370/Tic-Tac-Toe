bc51acac1c1af703a6b6497429e33d0d9497310b
Johann Mendoza <johannmendoza370@gmail.com>
2021-09-25 02:18:19 -0500

    Added player interaction with grid, an X or an O appears on click

--- Tic Tac Toe/MainWindow.xaml.cs
+++ Tic Tac Toe/MainWindow.xaml.cs
@@ -20,13 +20,55 @@ namespace Tic_Tac_Toe
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        string playerToMove;
+        string[,] grid;
+        int squaresLeft;
         public MainWindow()
         {
             InitializeComponent();
+            
+            startGame();
         }
 
-        private void changeColor(object sender, RoutedEventArgs e)
+        private void startGame()
         {
+            playerToMove = "Player 1";
+            grid = new string[3, 3] { { "", "", "" }, { "", "", "" }, { "", "", "" } };
+            squaresLeft = 9;
+        }
+
+        private void clickBox(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            changeText(button, playerToMove);
+            changePlayer();
+        }
+
+        private void changePlayer()
+        {
+            if (playerToMove == "Player 1")
+            {
+                playerToMove = "Player 2";
+            }
+            else if (playerToMove == "Player 2")
+            {
+                playerToMove = "Player 1";
+            }
+        }
+
+        private void changeText(Button button, string playerToMove)
+        {
+            if(playerToMove == "Player 1")
+            {
+                button.Content = "X";
+                playerLabel.Content = "Player 2's Turn";
+            }
+            else if (playerToMove == "Player 2")
+            {
+                button.Content = "O";
+                playerLabel.Content = "Player 1's Turn";
+            }
         }
     }
 }
